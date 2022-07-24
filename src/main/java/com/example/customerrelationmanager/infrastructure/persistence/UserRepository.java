dc1cda04018dbfdceee61515162d5bccea702fc4
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-24 02:06:17 -0500

    Refactor: remove unnecessary files

--- src/main/java/com/example/customerrelationmanager/infrastructure/persistence/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.customerrelationmanager.infrastructure.persistence;
-
-import com.example.customerrelationmanager.infrastructure.entity.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends MongoRepository<User, Long> {
-}
