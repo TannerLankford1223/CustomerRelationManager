b6c5ae71247074f35c430b91e7cff89806819f9f
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-24 02:32:02 -0500

    Add query for searching customers

--- src/main/java/com/example/customerrelationmanager/infrastructure/persistence/CustomerRepository.java
+++ src/main/java/com/example/customerrelationmanager/infrastructure/persistence/CustomerRepository.java
@@ -4,6 +4,10 @@ import com.example.customerrelationmanager.infrastructure.entity.Customer;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CustomerRepository extends MongoRepository<Customer, Integer> {
+
+    List<Customer> findAllByFirstNameOrderByLastName(String searchName);
 }
