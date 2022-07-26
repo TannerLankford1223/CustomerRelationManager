a66d6f6a1d132a6ee67dbfbe517d9bf0d130bc6a
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-26 03:24:33 -0500

    Add messages to validation annotations

--- src/main/java/com/example/customerrelationmanager/domain/dto/CustomerDTO.java
+++ src/main/java/com/example/customerrelationmanager/domain/dto/CustomerDTO.java
@@ -7,7 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
 @Data
@@ -18,19 +18,19 @@ public class CustomerDTO {
 
     private long customerId;
 
-    @NotNull
+    @NotEmpty(message = "Customer's first name can't be empty")
     private String firstName;
 
-    @NotNull
+    @NotEmpty(message = "Customer's last name can't be empty")
     private String lastName;
 
-    @Email(message = "Email must be valid")
-    @NotNull
+    @Email(message = "Customer's email must be valid")
+    @NotEmpty(message = "Customer's email can't be empty")
     private String email;
 
     @Pattern(regexp = "^(\\+\\d{1,3}( )?)?((\\(\\d{1,3}\\))|\\d{1,3})[- .]?\\d{3,4}[- .]?\\d{4}$",
-    message = "Please enter a valid phone number")
-    @NotNull
+    message = "Customer's phone number must be valid")
+    @NotEmpty(message = "Customer's phone number can't be empty")
     private String phone;
 
     private String notes;
