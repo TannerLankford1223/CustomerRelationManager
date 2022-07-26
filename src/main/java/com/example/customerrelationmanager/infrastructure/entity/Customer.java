14717dae0dc13e375756988070bde3ce871fd3cd
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-25 20:12:48 -0500

    Refactor: switch to a Postgres DB

--- src/main/java/com/example/customerrelationmanager/infrastructure/entity/Customer.java
+++ src/main/java/com/example/customerrelationmanager/infrastructure/entity/Customer.java
@@ -1,31 +1,34 @@
 package com.example.customerrelationmanager.infrastructure.entity;
 
-import com.example.customerrelationmanager.domain.dto.Note;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.persistence.*;
 
-@Document
+@Entity
+@Table
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Customer {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int customerId;
 
+    @Column
     private String firstName;
 
+    @Column
     private String lastName;
 
+    @Column
     private String email;
 
+    @Column
     private String phone;
 
-    private List<Note> notes = new ArrayList<>();
+    @Column
+    private String notes;
 }
--- src/main/java/com/example/customerrelationmanager/infrastructure/persistence/CustomerRepository.java
+++ src/main/java/com/example/customerrelationmanager/infrastructure/persistence/CustomerRepository.java
@@ -1,13 +1,13 @@
 package com.example.customerrelationmanager.infrastructure.persistence;
 
 import com.example.customerrelationmanager.infrastructure.entity.Customer;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CustomerRepository extends MongoRepository<Customer, Integer> {
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
     List<Customer> findAllByFirstNameOrderByLastName(String searchName);
 }
