04fedac3b770cdb57daf2cc627486c0c0a326bc9
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-26 03:58:04 -0500

    Add method to handle errors

--- src/main/java/com/example/customerrelationmanager/application/CustomerController.java
+++ src/main/java/com/example/customerrelationmanager/application/CustomerController.java
@@ -3,11 +3,14 @@ package com.example.customerrelationmanager.application;
 import com.example.customerrelationmanager.domain.dto.CustomerDTO;
 import com.example.customerrelationmanager.domain.ports.api.CustomerServicePort;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -98,6 +101,20 @@ public class CustomerController {
         model.addAttribute("customer", customer);
         log.info("notes updated successfully");
         return "note-form";
+    }
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error-404";
+            }
+        }
+        return "error";
     }
 
 }
