747f5f1ef7f91cffba667249eba3bb75ee2db6f1
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-26 03:25:05 -0500

    Implement methods

--- src/main/java/com/example/customerrelationmanager/application/CustomerController.java
+++ src/main/java/com/example/customerrelationmanager/application/CustomerController.java
@@ -1,11 +1,17 @@
 package com.example.customerrelationmanager.application;
 
+import com.example.customerrelationmanager.domain.dto.CustomerDTO;
 import com.example.customerrelationmanager.domain.ports.api.CustomerServicePort;
-import com.example.customerrelationmanager.infrastructure.entity.Customer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
@@ -16,44 +22,82 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
+    // Redirects to customer list page
+    @RequestMapping("/")
+    public String showPage() {
+        return "redirect:/customer/list";
+    }
+
     @GetMapping("/list")
     public String listCustomers(Model model) {
-        return null;
+        List<CustomerDTO> customers = customerService.getCustomers();
+
+        model.addAttribute("customers", customers);
+
+        return "list-customers";
     }
 
     @GetMapping("/showFormForAdd")
     public String showFormForAdd(Model model) {
-        return null;
+        CustomerDTO customer = new CustomerDTO();
+
+        model.addAttribute("customer", customer);
+
+        return "customer-form";
     }
 
     @PostMapping("/saveCustomer")
-    public String saveCustomer(@ModelAttribute("customer") Customer customer) {
-        return null;
+    public String saveCustomer(@ModelAttribute("customer") @Valid CustomerDTO customer, BindingResult result) {
+        if (result.hasErrors()) {
+            return "customer-form";
+        }
+
+        customerService.saveCustomer(customer);
+        log.info("customer saved successfully");
+        return "redirect:/customer/list";
     }
 
     @GetMapping("/details")
-    public String showFormForDetails(Model model) {
-        return null;
+    public String showFormForDetails(@RequestParam("customerId") int id, Model model) {
+        CustomerDTO customer = customerService.getCustomer(id);
+
+        model.addAttribute("customer", customer);
+
+        return "customer-details";
     }
 
     @GetMapping("/showFormForUpdate")
     public String showFormForUpdate(@RequestParam("customerId") int id, Model model) {
-        return null;
+        CustomerDTO customer = customerService.getCustomer(id);
+
+        model.addAttribute("customer", customer);
+        log.info("customer updated successfully");
+        return "customer-form";
     }
 
     @GetMapping("/deleteCustomer")
     public String deleteCustomer(@RequestParam("customerId") int id, Model model) {
-        return null;
+        customerService.deleteCustomer(id);
+        log.error("customer deleted successfully");
+        return "redirect:/customer/list";
     }
 
     @GetMapping("/search")
     public String searchCustomers(@RequestParam("searchName") String searchName, Model model) {
-        return null;
+        List<CustomerDTO> customers = customerService.searchCustomers(searchName);
+
+        model.addAttribute("customers", customers);
+
+        return "list-customers";
     }
 
     @GetMapping("/showFormForNotes")
     public String showFormForNotes(@RequestParam("customerId") int id, Model model) {
-        return null;
+        CustomerDTO customer = customerService.getCustomer(id);
+
+        model.addAttribute("customer", customer);
+        log.info("notes updated successfully");
+        return "note-form";
     }
 
 }
--- src/main/java/com/example/customerrelationmanager/domain/service/CustomerService.java
+++ src/main/java/com/example/customerrelationmanager/domain/service/CustomerService.java
@@ -3,41 +3,62 @@ package com.example.customerrelationmanager.domain.service;
 import com.example.customerrelationmanager.domain.dto.CustomerDTO;
 import com.example.customerrelationmanager.domain.ports.api.CustomerServicePort;
 import com.example.customerrelationmanager.domain.ports.spi.CustomerPersistencePort;
+import com.example.customerrelationmanager.infrastructure.entity.Customer;
+import com.example.customerrelationmanager.infrastructure.mapper.CustomerMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomerService implements CustomerServicePort {
 
     private final CustomerPersistencePort customerRepo;
 
-    public CustomerService(CustomerPersistencePort customerRepo) {
+    private final CustomerMapper mapper;
+
+    public CustomerService(CustomerPersistencePort customerRepo, CustomerMapper mapper) {
         this.customerRepo = customerRepo;
+        this.mapper = mapper;
     }
 
     @Override
     public List<CustomerDTO> getCustomers() {
-        return null;
+        return customerRepo.getCustomers().stream()
+                .map(mapper::customerToCustomerDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
+    @Transactional
     public void saveCustomer(CustomerDTO customer) {
-
+        customerRepo.saveCustomer(mapper.customerDTOToCustomer(customer));
     }
 
     @Override
     public CustomerDTO getCustomer(int id) {
-        return null;
+        Optional<Customer> customerOpt = customerRepo.getCustomer(id);
+
+        if (customerOpt.isPresent()) {
+            return mapper.customerToCustomerDTO(customerOpt.get());
+        }
+
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "customer not found");
     }
 
     @Override
+    @Transactional
     public void deleteCustomer(int id) {
-
+        customerRepo.deleteCustomer(id);
     }
 
     @Override
     public List<CustomerDTO> searchCustomers(String searchName) {
-        return null;
+        return customerRepo.searchCustomers(searchName).stream()
+                .map(mapper::customerToCustomerDTO).collect(Collectors.toList());
     }
 }
